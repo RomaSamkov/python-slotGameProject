471d55ff78418ba655b92fbceb4cfa9e710255dd
RomaSamkov <Romansamkov.rs@gmail.com>
2024-09-10 16:32:43 +0300

    add def print_slot_machine

--- main.py
+++ main.py
@@ -33,6 +33,16 @@ def get_slot_machine_spin(rows,cols, symbols):
 
     return columns
 
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
+
 
 def deposit():
     while True:
@@ -85,5 +95,7 @@ def main():
         else:
             break
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+    slots = get_slot_machine_spin(ROWS,COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
